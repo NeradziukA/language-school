5edff95946d2b0e50c2de77e1575b11ebd43dfa1
Think <alener@gmail.com>
2025-03-01 00:08:34 +0100

    fix import

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { languages } from "@/i18n/settings";
-import "./globals.css";
 import { Header, Footer } from "@/components";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
