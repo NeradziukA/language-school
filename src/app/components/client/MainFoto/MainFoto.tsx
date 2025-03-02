3bec7b06309cd17d1d3eb8c923c23b6fc8bf1d6a
Think <alener@gmail.com>
2025-03-02 23:41:20 +0100

    fix img selector

--- src/app/components/client/MainFoto/MainFoto.tsx
+++ src/app/components/client/MainFoto/MainFoto.tsx
@@ -5,7 +5,13 @@ import styles from "./MainFoto.module.css";
 export function MainFoto() {
   return (
     <aside className={styles.mainFoto}>
-      <Image src="/main-foto.png" alt="Main Foto" width={568} height={760} />
+      <Image
+        className={styles.img}
+        src="/main-foto.png"
+        alt="Main Foto"
+        width={568}
+        height={760}
+      />
     </aside>
   );
 }
