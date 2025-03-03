2e766786b8387ccd6b43dd5b83f370e6fc0b3756
Think <alener@gmail.com>
2025-03-03 20:52:00 +0100

    add animation

--- src/app/components/client/Content/Content.tsx
+++ src/app/components/client/Content/Content.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect } from "react";
 import { useCurrentLocale } from "@/app/locales/client";
 import styles from "./Content.module.css";
 import { loadApi, PageKey } from "@/app/api/loader";
@@ -12,15 +13,20 @@ export function Content({ pageKey }: Readonly<{ pageKey: PageKey }>) {
   const locale = useCurrentLocale();
   const content = loadApi(pageKey, locale);
 
+  useEffect(() => {
+    const offers = document.querySelectorAll(`.${styles.offer}`);
+    offers.forEach((offer, index) => {
+      setTimeout(() => {
+        offer.classList.add(styles.visible);
+      }, index * 300);
+    });
+  }, [content]);
+
   if (Array.isArray(content)) {
     return (
       <div className={`${styles.content} ${styles.fadeIn}`}>
         {content.map((item: Offer, index) => (
-          <div
-            key={index}
-            className={`${styles.offer} ${styles.slideUp}`}
-            style={{ animationDelay: `${index * 0.1}s` }}
-          >
+          <div key={index} className={`${styles.offer}`}>
             <h3 className={styles.title}>{item.title}</h3>
             <div className={styles.divider} />
             <p className={styles.description}>{item.content}</p>
