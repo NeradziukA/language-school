61c8526ffd88883cafa2fcf0092c2a5f0e0bfeb4
Aliaksandr Neradziuk <aliaksandr_neradziuk@epam.com>
2025-03-04 16:01:24 +0100

    add contacts

---
+++ src/app/api/contact.ts
@@ -0,0 +1,58 @@
+export const contacts: Record<string, { title: string; content: string }[]> = {
+  en: [
+    {
+      title: "Phone",
+      content: "+1-234-567-890",
+    },
+    {
+      title: "Telegram",
+      content: "@language_school_en",
+    },
+    {
+      title: "Email",
+      content: "contact@language-school.com",
+    },
+  ],
+  ru: [
+    {
+      title: "Телефон",
+      content: "+7-123-456-7890",
+    },
+    {
+      title: "Телеграм",
+      content: "@language_school_ru",
+    },
+    {
+      title: "Электронная почта",
+      content: "contact@language-school.ru",
+    },
+  ],
+  fr: [
+    {
+      title: "Téléphone",
+      content: "+33-1-23-45-67-89",
+    },
+    {
+      title: "Télégramme",
+      content: "@language_school_fr",
+    },
+    {
+      title: "Email",
+      content: "contact@language-school.fr",
+    },
+  ],
+  ka: [
+    {
+      title: "ტელეფონი",
+      content: "+995-32-123-4567",
+    },
+    {
+      title: "ტელეგრამი",
+      content: "@language_school_ka",
+    },
+    {
+      title: "ელ.ფოსტა",
+      content: "contact@language-school.ge",
+    },
+  ],
+};
